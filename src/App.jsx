617b6132b07d7672440e23cf2edfc9c2b1b0005d
ertyhd <orionship@protonmail.com>
2023-01-28 14:45:40 +0200

    Add localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,28 @@ import Filter from './components/Filter/Filter';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+
+  componentDidMount() {
+    const items = JSON.parse(localStorage.getItem('my-contacts'));
+    if (items?.length) {
+      this.setState({ items });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+      localStorage.setItem('my-contacts', JSON.stringify(contacts));
+    }
+  }
 
   addContact = (name, number) => {
     const { contacts } = this.state;
